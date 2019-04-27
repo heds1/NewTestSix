0abcb5af56a8b9d13e87a0308a8925e60e1c0e31
Hedley Stirrat <hedley.stirrat@gmail.com>
2019-04-27 18:26:36 +1200

    added movie model, including custom ClientSampleID string field.

---
+++ NewTestSix/Migrations/20190427060439_AddedClientSampleID_InMovieControllerOnly.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewTestSix.Migrations
+{
+    public partial class AddedClientSampleID_InMovieControllerOnly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSampleID",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientSampleID",
+                table: "Movie");
+        }
+    }
+}
--- NewTestSix/Models/SubmitSample.cs
+++ NewTestSix/Models/SubmitSample.cs
@@ -6,10 +6,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewTestSix.Models
 {
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string ClientSampleID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+
+
     public class SubmitSample
     {
         public int Id { get; set; }
         public string SampleID { get; set; }
+
+
+
+
     }
 
     public class RequestedAnalyses
